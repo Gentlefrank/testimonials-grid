a44894c40825fbea8f44bb4e2d441325af291093
apple <apple@apples-MacBook-Pro.local>
2024-12-05 09:59:26 +0100

    almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
 import logo from './logo.svg';
-import './App.css';
+ 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+    <div className='myGrid'>
+       <div className='item1'>
+       
+         <div className='item--header'>
+         <div ><img src='images/image-daniel.jpg' alt=''  className='item--image'/> </div>
+        <div><p className='fontBold '>Daniel Clifford</p><p className='opaque'>Verified Graduate</p> </div> 
+          </div>
+        <div className='item-content'>
+        <h1>
+        I received a job offer mid-course, and the subjects I learned were current, if
+        not more so, in the company I joined. I honestly feel I got every penny's worth.
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        "I was an EMT for many years before I joined the bootcamp. I've been looking to make 
+        a transition and have heard some people who had an amazing experience here. I signed up for the 
+        free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks 
+        was the best -  and most grueling - time of my life. Since completing the course, I've successfully 
+        switched careers working as a Sofware Engineer for a VR startup.  "
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+       
+       </div>
+       <div className='item2'>
+       <div className='item--header'>
+         <div ><img src='images/image-jonathan.jpg' alt=''  className='item--image'/> </div>
+        <div><p className='fontBold '>Jonathan Walters</p><p className='opaque'>Verified Graduate</p> </div> 
+          </div>
+        <div className='item-content'>
+        <h1>
+        The team was supportive and kept me motivated. 
+        </h1>
+        <p>
+        "I started as a total newbie with virtually no coding skills. I now work as a mobile
+        engineer for a big company. This was one of the best I've investment I've made in myself."
+        </p>
+        </div>
+       
+       </div>
+       <div className='item3'>
+       <div className='item--header'>
+         <div ><img src='images/image-jeanette.jpg' alt=''  className='item--image'/> </div>
+        <div><p className='fontBold '>Jeanette Harmon</p><p className='opaque'>Verified Graduate</p> </div> 
+          </div>
+        <div className='item-content'>
+        <h1>
+        An overall wonderful and rewarding experience
+        </h1>
+        <p>
+        "Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love."
+        </p>
+        </div>
+       </div>
+       <div className='item4'>
+       <div className='item--header'>
+         <div ><img src='images/image-patrick.jpg' alt=''  className='item--image'/> </div>
+        <div><p className='fontBold '>Patrick Abrams</p><p className='opaque'>Verified Graduate</p> </div> 
+          </div>
+        <div className='item-content'>
+        <h1>
+        Awesome teaching support from TAs who did the Bootcamp themselves. Getting guidance from them and learning
+        from their experiences was easy.
+        </h1>
+        <p>
+        "The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence
+        necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest.
+        You will get the personal attention you need from an incredible community of smart and amazing people."
+        </p>
+        </div>
+       </div>
+       <div className='item5'>
+       <div className='item--header'>
+         <div ><img src='images/image-patrick.jpg' alt=''  className='item--image'/> </div>
+        <div><p className='fontBold '>Patrick Abrams</p><p className='opaque'>Verified Graduate</p> </div> 
+          </div>
+        <div className='item-content'>
+        <h1>
+        Such a life-changing experience. Highly recommended!
+        </h1>
+        <p>
+        "Before joining the Bootcamp, I've never written a line of code. I needed some structure from professionals
+        who can help me learn programming step by step. I was encouraged to enrol by a former a student of theirs who
+        can only say wonderful things about the program. The entire curriculum and staff did not disappoint. The were very
+        hands-on and I never had to wait long for assistance. The agile team project, in particular, was outstanding. It took
+        my learning to the next level in a way that no tutorial could ever have. In fact, I've oftern referred to it duriing interviews
+        as an example of my development experience. It certainly help me land a job as a full-stack developer after receiving multiple offers. 
+        100% recommend! "
+        </p>
+        </div>
+       </div>
+       </div>
     </div>
   );
 }
